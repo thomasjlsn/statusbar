68334a120d597cb8442d413d90fcf1ea7ee78833
Thomas Ellison <thomasjlsn@gmail.com>
2020-07-22 16:39:07 -0700

    remove dependency on psutil for checking ram

--- statusdlib/stats/memory.py
+++ statusdlib/stats/memory.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 """Memory usage."""
 
-from psutil import virtual_memory
 from statusdlib.core.components import Component
 from statusdlib.core.ui import meter
 
 
 def memory_usage():
-    """Memory displayed in largest unit."""
-    mem = virtual_memory()._asdict()
+    with open('/proc/meminfo', 'r') as f:
+        meminfo = [l.strip() for l in f.readlines()]
+
+    mem = {}
+
+    for i in meminfo:
+        try:
+            key, val, _ = i.split()
+            key = key[:-1]
+        except ValueError:
+            try:
+                key, _ = i.split()
+                key = key[:-1]
+                val = 0
+            except ValueError:
+                continue
+        try:
+            mem[key] = int(val)
+        except TypeError:
+            continue
 
     return meter(
-        current=mem['used'],
-        maximum=mem['total'],
+        # memory usage as calculated by `free(1)`
+        current=(mem['MemTotal'] - mem['MemFree'] - mem['Buffers'] - mem['Cached']),
+        maximum=mem['MemTotal'],
     )
 
 
