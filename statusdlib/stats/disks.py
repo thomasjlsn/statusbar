b65c97dc53688730d22bc2a5b0aab5db5b1e0437
Thomas Ellison <thomasjlsn@gmail.com>
2020-07-22 22:08:15 -0700

    dynamically cycle through mounted disks
    now can handle disks being mounted / umounted while running and update
    self accordingly

--- statusdlib/stats/disks.py
+++ statusdlib/stats/disks.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Disk usage."""
 
-from itertools import cycle
 from os import popen
 
 from statusdlib.core.components import Component
@@ -9,15 +8,23 @@ from statusdlib.core.ui import meter
 
 
 class Block_Devices:
-    disks = cycle([
-        line.split()[0] for line in [
-            drive.strip() for drive in
-            popen('df').readlines() if drive.startswith('/dev/sda')
+    def disks(self):
+        return [
+            line.split()[0] for line in [
+                drive.strip() for drive in
+                popen('df').readlines() if drive.startswith('/dev/sd')
+            ]
         ]
-    ])
+
+    seen = set()
 
     def next(self):
-        return(next(self.disks))
+        while True:
+            for disk in self.disks():
+                if disk not in self.seen:
+                    self.seen.add(disk)
+                    return disk
+            self.seen = set()
 
 
 disks = Block_Devices()
