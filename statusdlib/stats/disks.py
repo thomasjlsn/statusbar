60b256399de311f490f9c681c05f1e1ff04a0685
Thomas Ellison <thomasjlsn@gmail.com>
2020-07-23 10:51:22 -0700

    remove dependency on `df` for checking disk size

--- statusdlib/stats/disks.py
+++ statusdlib/stats/disks.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 """Disk usage."""
 
-from os import popen
+from os import path, statvfs
 
 from statusdlib.core.components import Component
 from statusdlib.core.ui import meter
 
 
 class Block_Devices:
+    mntfile = '/proc/mounts'
+    if path.exists(mntfile):
+        if path.islink(mntfile):
+            mntfile = path.realpath(mntfile)
+
+    def mounted(self):
+        with open(self.mntfile) as mnt:
+            return [dev.strip() for dev in mnt.readlines()]
+
     def disks(self):
+        """Returns list of lists containing [disk, mountpoint]."""
         return [
-            line.split()[0] for line in [
-                drive.strip() for drive in
-                popen('df').readlines() if drive.startswith('/dev/sd')
-            ]
+            dev.split()[0:2] for dev in
+            self.mounted() if dev.startswith('/dev/sd')
         ]
 
     seen = set()
@@ -21,8 +29,8 @@ class Block_Devices:
     def next(self):
         while True:
             for disk in self.disks():
-                if disk not in self.seen:
-                    self.seen.add(disk)
+                if disk[0] not in self.seen:
+                    self.seen.add(disk[0])
                     return disk
             self.seen = set()
 
@@ -30,18 +38,31 @@ class Block_Devices:
 disks = Block_Devices()
 
 
+def size_of(mountpoint):
+    stats = statvfs(mountpoint)
+    block_size = stats.f_frsize
+    total_size = stats.f_blocks
+    free_size = stats.f_bfree
+    return (total_size * block_size,
+            free_size * block_size)
+
+
 def disk_usage():
     disk = disks.next()
 
-    label = f'{disk.split("/")[-1:][0].upper()}: '
+    label = path.basename(disk[0]).upper()
+    mountpoint = disk[1]
 
-    return label + meter(int(
-        popen(f'df {disk} --output=pcent').readlines()[1].strip()[:-1]
-    ))
+    size, free = size_of(mountpoint)
+
+    return ': '.join([label, meter(
+        current=(size - free),
+        maximum=size,
+    )])
 
 
 usage = Component(
     source=disk_usage,
-    sleep_ms=20000,
+    sleep_ms=5000,
     weight=85,
 )
