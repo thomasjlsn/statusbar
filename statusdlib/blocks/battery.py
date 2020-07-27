53996848a618335abb9f5005ce36ea96d00126a3
Thomas Ellison <thomasjlsn@gmail.com>
2020-07-27 14:05:19 -0700

    refactored battery.py

--- statusdlib/blocks/battery.py
+++ statusdlib/blocks/battery.py
@@ -3,38 +3,44 @@
 
 from glob import glob
 
-from statusdlib.components import Block, meter
+from statusdlib.components import Block, RemoveBlock, meter
 from statusdlib.helpers import readint
 
 
-def battery_source() -> str:
-    """Returns current battery percentage"""
+class Power:
     try:
         batteries = glob('/sys/class/power_supply/BAT*')
-
-        if not batteries:
-            return None
-        else:
-            power_levels = []
-            for battery in batteries:
-                full = readint(f'{battery}/energy_full')
-                now = readint(f'{battery}/energy_now')
-                power = ((100 / full) * now)
-                power_levels.append(power)
-
-        percent = int(sum(power_levels) / len(batteries))
-
-        charging = readint('/sys/class/power_supply/AC/online')
-
-        if charging:
-            if percent >= 99:
-                return f'{meter(percent)} full'
-            return f'{meter(percent)} ++'
-
-        return meter(percent)
-
     except FileNotFoundError:
-        return None
+        batteries = None
+
+    def power_levels(self):
+        for battery in self.batteries:
+            full = readint(f'{battery}/energy_full')
+            now = readint(f'{battery}/energy_now')
+            power = ((100 / full) * now)
+            yield power
+
+    def percent(self):
+        return int(sum(self.power_levels()) / len(self.batteries))
+
+    def charging(self):
+        return readint('/sys/class/power_supply/AC/online')
+
+
+power = Power()
+
+
+def battery_source():
+    if not power.batteries:
+        raise RemoveBlock
+
+    percent = power.percent()
+
+    if power.charging():
+        if percent >= 99:
+            return f'{meter(percent)} full'
+        return f'{meter(percent)} ++'
+    return meter(percent)
 
 
 def main():
