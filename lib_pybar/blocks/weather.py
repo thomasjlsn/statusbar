347c59baa8b3b46a4a37d24514f50ec9b4abe914
Thomas Ellison <thomasjlsn@gmail.com>
2020-11-08 22:53:00 -0800

    cache woeid(), added env var to specify C or F preference

--- lib_pybar/blocks/weather.py
+++ lib_pybar/blocks/weather.py
@@ -2,6 +2,7 @@
 
 import json
 import posixpath
+from functools import lru_cache
 from os import getenv
 
 from lib_pybar.core import Block
@@ -13,28 +14,39 @@ from requests import get
 PYBAR_LATT = getenv('PYBAR_LATT', 34.409211)
 PYBAR_LONG = getenv('PYBAR_LONG', -118.914837)
 
+# To specify celsius or fahrenheit
+PYBAR_TEMP_UNITS = getenv('PYBAR_UNITS', 'f').lower()[0]
+
 _weather_conditions = {
     # key: (show?, 'name')   # original condition name
     # ====================================================
-    'sn':  (1, 'snow'),      # Snow
-    'sl':  (1, 'sleet'),     # Sleet
-    'h':   (1, 'hail'),      # Hail
-    't':   (1, 'storm'),     # Thunderstorm
-    'hr':  (1, 'rain'),      # Heavy Rain
-    'lr':  (1, 'rain'),      # Light Rain
-    's':   (1, 'rain'),      # Showers
-    'hc':  (1, 'overcast'),  # Heavy Cloud
-    'lc':  (0, 'overcast'),  # Light Cloud
-    'c':   (0, 'clear'),     # Clear
+    'sn':  (1, 'snow'),    # Snow
+    'sl':  (1, 'sleet'),   # Sleet
+    'h':   (1, 'hail'),    # Hail
+    't':   (1, 'storm'),   # Thunderstorm
+    'hr':  (1, 'rain'),    # Heavy Rain
+    'lr':  (1, 'rain'),    # Light Rain
+    's':   (1, 'rain'),    # Showers
+    'hc':  (1, 'cloudy'),  # Heavy Cloud
+    'lc':  (0, 'cloudy'),  # Light Cloud
+    'c':   (0, 'clear'),   # Clear
 }
 
 
-def fahrenheit(celsius):
-    return (celsius * 9/5) + 32
-
-
 def getjson(url):
+    '''Get JSON from a given url.'''
     return json.loads(get(url).text)
+
+
+def celsius(C):
+    '''MetaWeather returns its data in celsius.'''
+    return f'{C}°C'
+
+
+def fahrenheit(temp):
+    '''Convert to fahrenheit and format.'''
+    F = int((temp * 9/5) + 32)
+    return f'{F}°F'
 
 
 class MetaWeatherSearchAPI:
@@ -44,29 +56,30 @@ class MetaWeatherSearchAPI:
         self.lat = lat
         self.lon = lon
 
-    def coords(self):
+    @lru_cache(maxsize=1)
+    def woeid(self):
         return getjson(posixpath.join(
             self.api_base, f'search/?lattlong={self.lat},{self.lon}'
-        ))
+        ))[0]['woeid']
 
     def weather(self):
         return getjson(posixpath.join(
-            self.api_base, str(self.coords()[0]['woeid'])
-        ))
-
-    def consolidated(self):
-        return self.weather()['consolidated_weather']
+            self.api_base, str(self.woeid())
+        ))['consolidated_weather']
 
 
 search = MetaWeatherSearchAPI(PYBAR_LATT, PYBAR_LONG)
 
+preferred_units = {
+    'c': celsius,
+    'f': fahrenheit,
+}[PYBAR_TEMP_UNITS]
+
 
 def check_weather():
-    today = search.consolidated()[0]
+    today = search.weather()[0]
 
-    _temp = int(fahrenheit(today['the_temp']))
-
-    temp = f'{_temp}°F'
+    temp = preferred_units(today['the_temp'])
 
     condition_is_noteworthy, condition = _weather_conditions[
         today['weather_state_abbr']
